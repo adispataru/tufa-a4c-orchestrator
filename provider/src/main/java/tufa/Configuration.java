6ffafcad7655c479e7db15a4f54aa372c5e274e3
Adrian Spataru <adi.spataru92@gmail.com>
2023-11-23 22:33:42 +0200

    minor deletions

--- provider/src/main/java/tufa/Configuration.java
+++ provider/src/main/java/tufa/Configuration.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-//@FormProperties({ "AIUrl", "dataBrokerUrl", "storageServiceUrl", "user", "password", "location", "kube", "kubeURL", "cacertData", "kubeToken", "kubeNamespace"})
 @FormProperties({ "AIUrl", "location", "kube", "kubeURL", "cacertData", "kubeToken", "kubeNamespace"})
 public class Configuration {
 
@@ -23,22 +22,7 @@ public class Configuration {
     @NotNull
     private String AIUrl;
 
-//    @FormLabel("Serrano Data Broker URL")
-//    @FormPropertyConstraint(pattern = "http\\:.+(?:\\d+)")
-//    @NotNull
-//    private String dataBrokerUrl;
 
-//    @FormLabel("Serrano Storage Service URL")
-//    @FormPropertyConstraint(pattern = "http\\:.+(?:\\d+)")
-//    @NotNull
-//    private String storageServiceUrl;
-
-//    @FormLabel("Serrano SDK User")
-//    private String user;
-
-//    @FormLabel("Serrano SDK Password")
-//    @FormPropertyDefinition(type = "string", isPassword = true)
-//    private String password;
 
     @FormLabel("Default Location")
     private String location;
@@ -56,16 +40,7 @@ public class Configuration {
     @FormLabel("Token")
     private String kubeToken;
 
-//    @FormLabel("Username")
-//    private String kubeUsername;
-
-
-
     @FormLabel("Namespace")
     private String kubeNamespace;
 
-
-
-//    @FormLabel("Tosca Metadata Providers")
-//    private List<String> providers;
 }
